b81c36e39e39dbd04e38b05b2825c16e5870cf3b
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-06-06 01:10:01 +0530

    class 1 | work done

---
+++ class1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './assets/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Helooooo Worldddd</h1>
+      <Nav />
+    </div>
+  )
+}
+
+export default App
